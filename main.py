4c7929ba42b6332cdc7fae96073e6559ffd1182b
nincompoop <nina.ayn.carrillo@gmail.com>
2024-04-03 18:33:43 -0700

    writting basics

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import pygame as pg
 from random import randrange
 
 WINDOW = 1000
+TILE_SIZE = 50
+RANGE = (TILE_SIZE // 2, WINDOW - TILE_SIZE // 2, TILE_SIZE)
+get_random_position = lambda: [randrange(*RANGE), randrange(*RANGE)]
+snake = pg. rect.RECT([0, 0, TILE_SIZE - 2,  TILE_SIZE - 2])
+snake.center = get_random_position
+
 screen = pg.display.set_mode([WINDOW] * 2)
 clock = pg.time.Clock()
 
