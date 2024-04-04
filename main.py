2b21a1820ff102894e4e1fd9d29b47123be52910
nincompoop <nina.ayn.carrillo@gmail.com>
2024-04-03 18:44:36 -0700

    move snake around screen

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ WINDOW = 1000
 TILE_SIZE = 50
 RANGE = (TILE_SIZE // 2, WINDOW - TILE_SIZE // 2, TILE_SIZE)
 get_random_position = lambda: [randrange(*RANGE), randrange(*RANGE)]
-snake = pg. rect.RECT([0, 0, TILE_SIZE - 2,  TILE_SIZE - 2])
-snake.center = get_random_position
-
+snake = pg. rect.Rect([0, 0, TILE_SIZE - 2,  TILE_SIZE - 2])
+snake.center = get_random_position()
+length = 1
+segments = [snake.copy()]
+snake_dir = (0,0)
 screen = pg.display.set_mode([WINDOW] * 2)
 clock = pg.time.Clock()
 
@@ -15,6 +17,17 @@ while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             exit()
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_w:
+                snake_dir = (0, -TILE_SIZE)
+            if event.key == pg.K_s:
+                snake_dir = (0, TILE_SIZE)
+            if event.key == pg.K_a:
+                snake_dir = (-TILE_SIZE, 0)
+            if event.key == pg.K_d:
+                snake_dir = (TILE_SIZE, 0)
     screen.fill('black')
+    # draw snake
+    [pg.draw.rect(screen, 'green', segment) for segment in segments]
     pg.display.flip()
     clock.tick(60)
